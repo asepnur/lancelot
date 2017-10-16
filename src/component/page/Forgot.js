2a4243efe79e997c709f18e433b7256e3f0c1a38
Risal Falah <risal@live.com>
2017-10-16 22:48:08 +0700

    add forgot password

--- src/component/page/Forgot.js
+++ src/component/page/Forgot.js
@@ -1,44 +1,90 @@
 import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 
-import {
-    LayoutGuest,
-    InputContent
-} from '../index.js'
+import {LayoutGuest, InputContent} from '../index.js'
 
 class Forgot extends Component {
-  render() {
-      return(
-          <LayoutGuest>
-        <div className="_bl5c">
-        </div>
-      <form className="_cn" action="/" method="POST">
-        <div className="_ro">
-          <div className="_c5m310 _c5m3o3 _c5x3o1 _c5x310">
-            <h2 className="_he3m">Forgot Password</h2>
-          </div>
-        </div>
-        <div className="_ro">
-          <div className="_c5m38 _c5m3o3 _c5x3o1 _c5x310">
-              <p className="_me3l">We will send you a link to reset your password</p>
-          </div>
-        </div>
-        <div className="_ro">
-            <InputContent classWraper="_c5m36 _c5m3o3 _c5x3o1 _c5x310" type="text" name="password" placeholder="Registered Email" />
-        </div>
-        <div className="_ro">
-            <div className="_c5m3o3 _c5m33 _c5x3o1  _c5x35">
-                <button className="_bt5m3m _pl5r" type="submit">Send</button>
-            </div>
-        </div>
-        <div className="_ro">
-          <div className="_c5m3o3 _c5m36 _c5x3o2 _c5x38">
-            <p className="_me5f">Remember your password? <b><Link to={'/login'}>Login</Link></b></p>
-          </div>
-        </div>
-    </form>
-  </LayoutGuest>
-      )
-  }
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            success: false
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        let formData = new FormData()
+        formData.append('email', this.state.email)
+        formData.append('resend', 'true')
+        fetch('https://meikoapp.herokuapp.com/api/v1/user/forgot', {
+            method: 'POST',
+            credentials: 'include',
+            crossDomain: true,
+            body: formData
+        }).then((res) => {
+            return res.json()
+        }).then((data) => {
+            if (data.code === 200) {
+                this.setState({
+                    success: true
+                })
+            }
+        })
+    }
+
+    render() {
+        let url = '/forgot/' + this.state.email
+        return (this.state.success ? <Redirect to={url}/> : this.renderMain())
+    }
+
+    renderMain() {
+        return (
+            <LayoutGuest>
+                <div className="_bl5c"></div>
+                <form className="_cn" onSubmit={this.handleSubmit}>
+                    <div className="_ro">
+                        <div className="_c5m310 _c5m3o3 _c5x3o1 _c5x310">
+                            <h2 className="_he3m">Forgot Password</h2>
+                        </div>
+                    </div>
+                    <div className="_ro">
+                        <div className="_c5m38 _c5m3o3 _c5x3o1 _c5x310">
+                            <p className="_me3l">We will send you a link to reset your password</p>
+                        </div>
+                    </div>
+                    <div className="_ro">
+                        <InputContent
+                            classWraper="_c5m36 _c5m3o3 _c5x3o1 _c5x310"
+                            type="text"
+                            name="email"
+                            placeholder="Email"
+                            onChangeState={this.handleChange}
+                            value={this.state.email}/>
+                    </div>
+                    <div className="_ro">
+                        <div className="_c5m3o3 _c5m33 _c5x3o1  _c5x35">
+                            <button className="_bt5m3m _pl5r" type="submit">Send</button>
+                        </div>
+                    </div>
+                    <div className="_ro">
+                        <div className="_c5m3o3 _c5m36 _c5x3o2 _c5x38">
+                            <p className="_me5f">Remember your password?
+                                <b>
+                                    <Link to={'/login'}>Login</Link>
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </LayoutGuest>
+        )
+    }
 }
 export default Forgot
--- src/component/page/ForgotActivation.js
+++ src/component/page/ForgotActivation.js
@@ -3,20 +3,17 @@ import {Link, Redirect} from 'react-router-dom'
 
 import {LayoutGuest, InputContent} from '../index.js'
 
-class Reset extends Component {
+class ForgotActivation extends Component {
     constructor() {
         super()
         this.state = {
             email: '',
-            code: '',
-            password: '',
-            password_confirmation: '',
-            success: false
+            code: ''
         }
     }
 
     componentWillMount() {
-        this.setState({email: this.props.match.params.email, code: this.props.match.params.code})
+        this.setState({email: this.props.match.params.email})
     }
 
     handleChange = (e) => {
@@ -27,15 +24,9 @@ class Reset extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-        if (this.state.password !== this.state.password_confirmation) {
-            console.log('password is not match')
-            return
-        }
-
         let formData = new FormData()
         formData.append('email', this.state.email)
         formData.append('code', this.state.code)
-        formData.append('password', this.state.password)
         fetch('https://meikoapp.herokuapp.com/api/v1/user/forgot', {
             method: 'POST',
             credentials: 'include',
@@ -51,9 +42,8 @@ class Reset extends Component {
     }
 
     render() {
-        return (this.state.success
-            ? <Redirect to='/login'/>
-            : this.renderMain())
+        let url = '/reset/' + this.state.email + '/' + this.state.code
+        return (this.state.success ? <Redirect to={url}/> : this.renderMain())
     }
 
     renderMain() {
@@ -62,30 +52,34 @@ class Reset extends Component {
                 <div className="_bl5c"></div>
                 <form className="_cn" onSubmit={this.handleSubmit}>
                     <div className="_ro">
-                        <div className="_c5m310 _c5m3o3 _c5x3o1 _c5x310">
-                            <h2 className="_he3m">Reset Password</h2>
-                        </div>
-                        <InputContent
-                            classWraper="_c5m36 _c5m3o3 _c5x3o1 _c5x310"
-                            type="password"
-                            name="password"
-                            placeholder="Enter new password"
-                            onChangeState={this.handleChange}
-                            value={this.state.password}/>
-                        <InputContent
-                            classWraper="_c5m36 _c5m3o3 _c5x3o1 _c5x310"
-                            type="password"
-                            name="password_confirmation"
-                            placeholder="Reenter new password"
-                            onChangeState={this.handleChange}
-                            value={this.state.password_confirmation}/>
-                        <div className="_c5m3o3 _c5m33 _c5x3o1  _c5x35">
-                            <button className="_bt5m3m _pl5r" type="submit">Send</button>
+                        <div className="_c5m310 _c5m3o1 _c5x312">
+                            <h2 className="_he3cm">Thank you for your registration!</h2>
                         </div>
                     </div>
                     <div className="_ro">
-                        <div className="_c5m3o3 _c5m36 _c5x3o2 _c5x38">
-                            <p className="_me5f">Remember your password?
+                        <div className="_c5m38 _c5m3o2 _c5x3o1 _c5x310">
+                            <p className="_me3v">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                        </div>
+                    </div>
+                    <div className="_ro">
+                        <InputContent
+                            classWraper="_c5m36 _c5m3o3 _c5x3o1 _c5x310"
+                            type="text"
+                            name="code"
+                            placeholder="Activation Code"
+                            onChangeState={this.handleChange}
+                            value={this.state.code}/>
+                    </div>
+                    <div className="_ro">
+                        <div className="_c5m3o3 _c5m33 _c5x3o3  _c5x36">
+                            <button className="_bt5m3m" type="submit">Activation</button>
+                        </div>
+                    </div>
+                    <div className="_ro">
+                        <div className="_c5m3o3 _c5m36 _c5x3o3 _c5x36">
+                            <p className="_me5f">
+                                Have an account?
                                 <b>
                                     <Link to={'/login'}>Login</Link>
                                 </b>
@@ -97,4 +91,5 @@ class Reset extends Component {
         )
     }
 }
-export default Reset
+
+export default ForgotActivation
