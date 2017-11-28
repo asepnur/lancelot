38c3e97266dffe5f9a65fd308478f5e5fa819abe
klikkanan <asepnur.isk@gmail.com>
2017-11-28 23:34:05 +0700

    grade

--- src/component/page/Grade.js
+++ src/component/page/Grade.js
@@ -1,99 +1,108 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
-import {
-    Navbar,
-    Newsbar,
-    LayoutUser
-} from '../index.js'
+import {Navbar, Newsbar, LayoutUser} from '../index.js'
 
-class Grade extends Component{
-    render(){
-        return(
+class Grade extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: []
+        }
+    }
+    componentDidMount() {
+        axios.get(`/api/v1/course/grade`, {
+            validateStatus: (status) => {
+                return status === 200
+            }
+        }).then((res) => {
+            this.setState({data: res.data.data})
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+    render() {
+        console.log(this.state.data)
+        return (
             <LayoutUser>
-            <Navbar match={this.props.match}/>
-            <div className="_cn">
-            <div className="_ro">
-                <div className="_pd5m3n _c5m312 _c5x312">
-                    <h1 className="_he3b">User Report</h1>
-                </div>
-            </div>
-            </div>
-            <div className="_cn">
-            <div className="_ro">
-                <div className="_c5m38 _c5x312 _pd5m3n">
-                    <div className="_se">
-                        <div className="_ro _pd3n3b">
-                            <div className="_c5m312 _c5x312 _pd3n3lr _pdx3n">
-                                <table className="_tb">
-                                    <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Mata Kuliah</th>
-                                        <th>Tugas</th>
-                                        <th>QUIS</th>
-                                        <th>UTS</th>
-                                        <th>UAS</th>
-                                        <th>Final Score</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Algoritma Pemrograman</td>
-                                        <td>90</td>
-                                        <td>75</td>
-                                        <td>85</td>
-                                        <td>70</td>
-                                        <td>80</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Lorem Ipsum</td>
-                                        <td>90</td>
-                                        <td>90</td>
-                                        <td>80</td>
-                                        <td>80</td>
-                                        <td>85</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Lorem Ipsum</td>
-                                        <td>90</td>
-                                        <td>90</td>
-                                        <td>80</td>
-                                        <td>80</td>
-                                        <td>85</td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>Lorem Ipsum</td>
-                                        <td>90</td>
-                                        <td>90</td>
-                                        <td>80</td>
-                                        <td>80</td>
-                                        <td>85</td>
-                                    </tr>
-                                    <tr>
-                                        <td>5</td>
-                                        <td>Lorem Ipsum</td>
-                                        <td>90</td>
-                                        <td>90</td>
-                                        <td>80</td>
-                                        <td>80</td>
-                                        <td>85</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                <Navbar match={this.props.match}/>
+                <div className="_ro _ma3mn">
+                    <div className="_cn">
+                        <div className="_ro">
+                            <div className="_c5m38 _c5x312 _pd5m3n">
+                                <div className="_he3b _pd3l3b">User Report</div>
+                                <Content data={this.state.data}/>
                             </div>
+                            <Newsbar/>
                         </div>
                     </div>
                 </div>
-                <Newsbar/>
-            </div>
-        </div>
             </LayoutUser>
         )
-    }    
+    }
+}
+const Content = (props) => {
+    return props.data.length === 0
+        ? <div>kosong</div>
+        : <table className="_se3g">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>
+                        <i className="fa fa-bookmark" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>Subject</span>
+                    </th>
+                    <th>
+                        <i className="fa fa-user" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>Attendance</span>
+                    </th>
+                    <th>
+                        <i className="fa fa-file-text" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>Assignment</span>
+                    </th>
+                    <th>
+
+                        <i className="fa fa-usd" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>QUIS</span>
+                    </th>
+                    <th>
+                        <i className="fa fa-star-half-o" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>MID</span>
+                    </th>
+                    <th>
+                        <i className="fa fa-star" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>FINAL</span>
+                    </th>
+                    <th>
+                        <i className="fa fa-graduation-cap" aria-hidden="true"></i>
+                        &nbsp;
+                        <span>Total</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.data
+                    .schedules
+                    .map((data, i) => (
+                        <tr key={i}>
+                            <td>{i+1}</td>
+                            <td>{data.name}</td>
+                            <td>75</td>
+                            <td>75</td>
+                            <td>85</td>
+                            <td>-</td>
+                            <td>80</td>
+                            <td>{data.total}</td>
+                        </tr>
+                    ))}
+            </tbody>
+        </table>
 }
 
 export default Grade
--- src/component/page/Schedule.js
+++ src/component/page/Schedule.js
@@ -1,106 +1,72 @@
 import React, {Component} from 'react'
 
-import {
-    Navbar,
-    Newsbar,
-    LayoutUser
-} from '../index.js'
+import {Navbar, Newsbar, LayoutUser} from '../index.js'
 
-class Schedule extends Component{
-    render(){
-        return(
+class Schedule extends Component {
+    render() {
+        return (
             <LayoutUser>
-            <Navbar match={this.props.match}/>
-            
-            <div className="_cn">
-            <div className="_ro">
-                <div className="_pd5m3n _c5m312 _c5x312">
-                    <h1 className="_he3b">Schedule</h1>
-                </div>
-            </div>
-        </div>
-        <div className="_cn">
-            <div className="_ro">
-                <div className="_c5m38 _pd5n _pd3cl _pd5m3n">
-                    <div className="_ta">
-                        <ul className="_ta5l">
-                            <li><a className="_ta5l3a" href="">Day</a></li>
-                            <li><a href="">Week</a></li>
-                            <li><a href="">Month</a></li>
-                        </ul>
-                        <div className="_ta5c">
-                            <div className="_se _se3a">
-                                <div className="_ro">
-                                    <div className="_c5x39 _c5m310">
-                                        <p className="_he3m3b">11.00 AM - 01.00 PM</p>
-                                        <p className="_he3x3bk">Algoritma Pemrograman</p>
-                                        <p className="_ct3s">Laboratorium Pemrograman / Gedung PPBSD</p>
-                                    </div>
-                                    <div className="_c5x33 _c5m32 _ma3xl3t ">
-                                        <i className="fa fa-star _ic3ms" aria-hidden="true"></i>
-                                        <i className="fa fa-ellipsis-h _ic3ms" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x312">
-                                        <hr/>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x39 _c5m310">
-                                        <p className="_he3m3b">11.00 AM - 01.00 PM</p>
-                                        <p className="_he3x3bk">Algoritma Pemrograman</p>
-                                        <p className="_ct3s">Laboratorium Pemrograman / Gedung PPBSD</p>
-                                    </div>
-                                    <div className="_c5x33 _c5m32 _ma3xl3t">
-                                        <i className="fa fa-star _ic3ms" aria-hidden="true"></i>
-                                        <i className="fa fa-ellipsis-h _ic3ms" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x312">
-                                        <hr/>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x39 _c5m310">
-                                        <p className="_he3m3b">11.00 AM - 01.00 PM</p>
-                                        <p className="_he3x3bk">Algoritma Pemrograman</p>
-                                        <p className="_ct3s">Laboratorium Pemrograman / Gedung PPBSD</p>
-                                    </div>
-                                    <div className="_c5x33 _c5m32 _ma3xl3t ">
-                                        <i className="fa fa-star _ic3ms" aria-hidden="true"></i>
-                                        <i className="fa fa-ellipsis-h _ic3ms" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x312">
-                                        <hr/>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x39 _c5m310">
-                                        <p className="_he3m3b">11.00 AM - 01.00 PM</p>
-                                        <p className="_he3x3bk">Algoritma Pemrograman</p>
-                                        <p className="_ct3s">Laboratorium Pemrograman / Gedung PPBSD</p>
-                                    </div>
-                                    <div className="_c5x33 _c5m32 _ma3xl3t">
-                                        <i className="fa fa-star _ic3ms" aria-hidden="true"></i>
-                                        <i className="fa fa-ellipsis-h _ic3ms" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div className="_ro">
-                                    <div className="_c5x312">
-                                        <hr/>
-                                    </div>
-                                </div>
-                            </div>
+                <Navbar match={this.props.match}/>
+                <div className="_cn">
+                    <div className="_ro _ma3mn">
+                        <div className="_c5m38 _pd5n _pd3cl _pd5m3n">
+                            <div class="_he3b">Schedule</div>
+                            <table class="_se _se3s">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <p>Wednesday
+                                                <span> At </span>
+                                                10.30-12.00</p>
+                                            <p>
+                                                <i class="fa fa-bookmark" aria-hidden="true"></i>
+                                                &nbsp;Algoritma Pemrograman</p>
+                                            <p>
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                                &nbsp;Laboratorium Pemrograman/ UDJT12</p>
+                                        </td>
+                                        <td>
+                                            <i class="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p>Wednesday
+                                                <span> At </span>
+                                                10.30-12.00</p>
+                                            <p>
+                                                <i class="fa fa-bookmark" aria-hidden="true"></i>
+                                                &nbsp;Algoritma Pemrograman</p>
+                                            <p>
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                                &nbsp;Laboratorium Pemrograman/ UDJT12</p>
+                                        </td>
+                                        <td>
+                                            <i class="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p>Wednesday
+                                                <span> At </span>
+                                                10.30-12.00</p>
+                                            <p>
+                                                <i class="fa fa-bookmark" aria-hidden="true"></i>
+                                                &nbsp;Algoritma Pemrograman</p>
+                                            <p>
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                                &nbsp;Laboratorium Pemrograman/ UDJT12</p>
+                                        </td>
+                                        <td>
+                                            <i class="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
+                        <Newsbar/>
                     </div>
-                </div> 
-                <Newsbar/>
-            </div>
-        </div>
+                </div>
 
             </LayoutUser>
         )
