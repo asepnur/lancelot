6ddfa827660315a2a698dcf7fb8d7354e33ddc74
klikkanan <asepnur.isk@gmail.com>
2017-11-28 19:28:28 +0700

    latest news get:

--- src/component/features/Newsbar.js
+++ src/component/features/Newsbar.js
@@ -1,82 +1,55 @@
 import React, {Component} from 'react'
-//import {Switch} from 'react-router'
+import axios from 'axios'
 
 class Newsbar extends Component {
+    constructor(){
+        super()
+        this.state = {
+            data:[]
+        }
+    }
     componentDidMount(){
-        
+        axios.get(`/api/v1/information`, {
+            validateStatus: (status) => {
+                return status === 200
+            }
+        }).then((res) => {
+            this.setState({data: res.data.data.last})
+        }).catch((err) => {
+            console.log(err)
+        })
     }
     render() {
         return (
             <div className="_c5m34 _c5x312 _pd3cr">
                 <div className="_he3b">Last News</div>
-                <table className="_se3inf">
-                    <tbody>
-                        <tr>
-                            <td><img src="/img/course.png" alt="informations"/></td>
-                            <td>
-                                <p>Lorem Ipsum Bla bla bala</p>
-                                <p>This is description from
-                                    fdfndskfsmfkdsmfdksfsmdfdsfdmsfd,smfd,sfmdsf,smf,sfmds,fmsk</p>
-                                <p className="_pd3m3t">3 Desember 2017</p>
-                            </td>
-                            <td>
-                                <i className="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="/img/course.png" alt="informations"/></td>
-                            <td>
-                                <p>Lorem Ipsum Bla bla bala</p>
-                                <p>This is description from
-                                    fdfndskfsmfkdsmfdksfsmdfdsfdmsfd,smfd,sfmdsf,smf,sfmds,fmsk</p>
-                                <p className="_pd3m3t">3 Desember 2017</p>
-                            </td>
-                            <td>
-                                <i className="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="/img/course.png" alt="information"/></td>
-                            <td>
-                                <p>Lorem Ipsum Bla bla bala</p>
-                                <p>This is description from
-                                    fdfndskfsmfkdsmfdksfsmdfdsfdmsfd,smfd,sfmdsf,smf,sfmds,fmsk</p>
-                                <p className="_pd3m3t">3 Desember 2017</p>
-                            </td>
-                            <td>
-                                <i className="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="/img/course.png" alt="informations"/></td>
-                            <td>
-                                <p>Lorem Ipsum Bla bla bala</p>
-                                <p>This is description from
-                                    fdfndskfsmfkdsmfdksfsmdfdsfdmsfd,smfd,sfmdsf,smf,sfmds,fmsk</p>
-                                <p className="_pd3m3t">3 Desember 2017</p>
-                            </td>
-                            <td>
-                                <i className="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><img src="/img/course.png" alt="informations"/></td>
-                            <td>
-                                <p>Lorem Ipsum Bla bla bala</p>
-                                <p>This is description from
-                                    fdfndskfsmfkdsmfdksfsmdfdsfdmsfd,smfd,sfmdsf,smf,sfmds,fmsk</p>
-                                <p className="_pd3m3t">3 Desember 2017</p>
-                            </td>
-                            <td>
-                                <i className="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Content data={this.state.data}/>
             </div>
 
         )
     }
 }
 
+const Content = (props)=>{
+    return props.data.length===0?<div>kosong</div>
+    :<table className="_se3inf">
+    <tbody>
+        {
+            props.data.map((data, i) =>(
+                <tr key={i}>
+                <td><img src="/img/course.png" alt="informations"/></td>
+                <td>
+                    <p>{data.title}</p>
+                    <p>{data.description}</p>
+                    <p className="_pd3m3t">{data.date}</p>
+                </td>
+                <td>
+                    <i className="fa fa-angle-double-right _ic __wr" aria-hidden="true"></i>
+                </td>
+            </tr>
+            ))
+        }
+    </tbody>
+</table>
+}
 export default Newsbar
